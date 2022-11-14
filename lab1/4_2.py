c84f31126d26f93f108593f6b6cf2511a308a482
qorganbek <dimashqorganbek@gmail.com>
2022-11-14 23:15:01 +0600

    upd

---
+++ lab1/4_2.py
@@ -0,0 +1,10 @@
+# a = []
+# for x in range(9, 18):
+#     a.append(x)
+#
+# n = int(input())
+# print(a[n - 1], end = " ")
+# c = (45 - (int(n/2)*10))
+# print(f'{c:02}')
+# a = 3.3335
+# print(f'{a:.2f}')
